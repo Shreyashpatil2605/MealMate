000338a9831b46202e8e1d2d8f8c8b28b48cb1a5
Shreyash Patil <shreyashpatil2605@gmail.com>
2026-02-27 09:45:25 +0530

    Small changes

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -8,13 +8,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDC1MLvqQXw2kL8vvoC9PduBFUu7SeC7LM",
-  authDomain: "mealmate-6095b.firebaseapp.com",
-  projectId: "mealmate-6095b",
-  storageBucket: "mealmate-6095b.firebasestorage.app",
-  messagingSenderId: "859545104350",
-  appId: "1:859545104350:web:6752ed5286d23926abf74f",
-  measurementId: "G-F86P7KW5XE",
+  apiKey: import.meta.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
